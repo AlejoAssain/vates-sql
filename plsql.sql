442cead2bed2d9b03aeb08aad7a6ea6af07b47b5
alejo-desktop <79205@sistemas.frc.utn.edu.ar>
2023-06-11 20:41:48 -0300

    11 de mayo

--- plsql.sql
+++ plsql.sql
@@ -1,6 +1,6 @@
 -- EJERCICIO PL/SQL
 -- E1
-CREATE TABLE CLIENTES_AA AS 
+CREATE TABLE CLIENTES_AA AS
        SELECT * FROM CLIENTES_BU;
 
 ALTER TABLE CLIENTES_AA
@@ -16,7 +16,7 @@ ALTER TABLE CELULARES_AA
       ADD CONSTRAINT celulares_aa_clientes_aa_fk
           FOREIGN KEY (id_cliente)
           REFERENCES CLIENTES_AA (id);
-          
+
 -- E2
 SELECT 'MI PRIMER ' || 'EJERCICIO' FROM DUAL;
 
@@ -24,13 +24,13 @@ SELECT 'MI PRIMER ' || 'EJERCICIO' FROM DUAL;
 SELECT UPPER('mi primier ejercicio en clase') FROM DUAL;
 
 -- E4
-SELECT LTRIM('    mi primer ejercicio en clase') FROM DUAL; 
+SELECT LTRIM('    mi primer ejercicio en clase') FROM DUAL;
 
 -- E5
 SELECT SUBSTR('mi primer ejercicio', -5) FROM DUAL;
 
 -- E6
-SELECT RTRIM('mi primer ejercicio en clase   ') FROM DUAL; 
+SELECT RTRIM('mi primer ejercicio en clase   ') FROM DUAL;
 
 -- E7
 SELECT LOWER('Mi primier ejercicio en clase') FROM DUAL;
@@ -51,7 +51,7 @@ SELECT CEIL(21.46) FROM DUAL;
 SELECT FLOOR(21.46) FROM DUAL;
 
 -- E13
-SELECT MOD(24, 5) FROM DUAL; 
+SELECT MOD(24, 5) FROM DUAL;
 
 -- E14
 SELECT POWER(8, 3) FROM DUAL;
@@ -89,14 +89,14 @@ BEGIN
 END;
 
 -- E22
-DECLARE 
+DECLARE
    v_numerica number := &num;
 BEGIN
    dbms_output.put_line(v_numerica);
 END;
 
 -- E23
-DECLARE 
+DECLARE
    v_p1 varchar2(20) := 'palabra 1';
    v_p2 varchar2(20) := 'palabra 2';
 BEGIN
@@ -133,29 +133,74 @@ BEGIN
   END LOOP;
 END;
 
+-- E27
+DECLARE
+  v_num number := 20;
+BEGIN
+  if v_num > 10 then
+    dbms_output.put_line('El numero es mayor a 10');
+  else
+    dbms_output.put_line('El numero no es mayor a 10');
+  end if;
+END;
+
+-- E28
+DECLARE
+  v_num number := &num1
+BEGIN
+  if v_num > 50 then
+    dbms_output.put_line('El valor es: ' || v_num);
+  else
+    dbms_output.put_line('El valor es menor a 50');
+  end if;
+END;
+
+-- E29
+DECLARE
+  v_num number := &num1;
+BEGIN
+  if v_num < 0 then
+    dbms_output.put_line('Valor anterior: ' || TO_CHAR(v_num));
+    dbms_output.put_line('Valor actual: ' || TO_CHAR(ABS(v_num)));
+  else
+    dbms_output.put_line('Valor: ' || TO_CHAR(v_num));
+  end if;
+END;
+
+-- E30
+DECLARE
+  v_num1 number := &num1;
+  v_num2 number := &num2;
+  v_sum number;
+BEGIN
+  v_sum = v_num1 + v_num2;
+
+  if v_sum < 51
+END;
+
 -- E32
 DECLARE
    v_cliente CLIENTES_AA%ROWTYPE;
-BEGIN 
-  SELECT * 
+BEGIN
+  SELECT *
   INTO v_cliente
   FROM CLIENTES_AA C
   WHERE C.PROVINCIA = 'Chaco';
-  
+
   if v_cliente.sueldo_neto > 50000 then
     v_cliente.sueldo_neto := v_cliente.sueldo_neto + 2;
   elsif v_cliente.sueldo_neto > 30000 then
     v_cliente.sueldo_neto := v_cliente.sueldo_neto + 10;
   else
     v_cliente.sueldo_neto := v_cliente.sueldo_neto + 14;
-    
+
   end if;
-  
+
   dbms_output.put_line(v_cliente.nombre);
   dbms_output.put_line(v_cliente.apellido);
   dbms_output.put_line(v_cliente.dni);
   dbms_output.put_line(v_cliente.sueldo_neto);
   dbms_output.put_line(v_cliente.provincia);
 END;
-    
-                         
+
+
